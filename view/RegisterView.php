e7cdc7575d67c213d2b51c4f807a5c41afa6ed5f
MosaKasem <thescorpion119@gmail.com>
2018-09-17 08:23:36 +0200

    Trim doesnt remove spaces inbetween

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -13,7 +13,10 @@ class RegisterView {
 		$message = '';
 
 		$response =	$this->generateRegisterFormHTML($message);
-		var_dump($_POST[self::$register]);
+		if(isset($_POST[self::$register])) {
+			$userRequest = $this->formValidation();
+			var_dump($userRequest);
+		}
 
 		return $response;
 	}
@@ -40,7 +43,35 @@ class RegisterView {
 			</form>
 		';
 	}
-	private function registerUser() {
-		echo $_POST;
+	private function formValidation() {
+		if ($_POST[self::$username] != trim($_POST[self::$username])) {
+			echo FALSE;
+		}
+		// COPY AND PASTE FROM FUNCTION.TRIM.PHP
+		// THIS FUCKING SUCKS
+		$text   = "\t\tThese are a few words :) ...  " . "<br>";
+		$binary = "\x09Example string\x0A" . "<br>";
+		$hello  = "Hello World" . "<br>";
+		var_dump($text, $binary, $hello);
+
+		print "\n";
+
+		$trimmed = trim($text);
+		var_dump($trimmed);
+
+		$trimmed = trim($text, " \t.") . "<br>";
+		var_dump($trimmed);
+
+		$trimmed = trim($hello, "Hdle");
+		var_dump($trimmed);
+
+		$trimmed = trim($hello, 'HdWr');
+		var_dump($trimmed);
+
+		// trim the ASCII control characters at the beginning and end of $binary
+		// (from 0 to 31 inclusive)
+		$clean = trim($binary, "\x00..\x1F");
+		var_dump($clean);
+		return strip_tags($_POST[self::$username]) && strip_tags($_POST[self::$password]) && strip_tags($_POST[self::$password2]);
 	}
 }
