790c6bcbc3d0978845d93f5b739fcac5feb57b77
MosaKasem <thescorpion119@gmail.com>
2018-09-24 12:24:50 +0200

    fix

--- view/RegisterView.php
+++ view/RegisterView.php
@@ -65,9 +65,10 @@ class RegisterView {
 			$this->setMessage("Username contains invalid characters.");
 			return false;
 		}
-/* 		if (strlen($userName) < 3 && strlen($passWord) < 6) {
-			$this->setMessage('Username has too few characters, at least 3 characters.');
-		} */
+		if (empty($userName) && empty($passWord)) {
+			$this->setMessage('Username has too few characters, at least 3 characters. Password has too few characters, at least 6 characters.');
+			return false;
+		}
 		if (empty($password) || strlen($passWord) < 6) {
 			$this->setMessage('Password has too few characters, at least 6 characters.');
 			return false;
